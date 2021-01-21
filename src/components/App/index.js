17265e2114d11a8b2db931eb138f97475d4b5188
Tan Dang <tandm160797@gmail.com>
2021-01-21 15:17:45 +0700

    Project setup structure

---
+++ src/components/App/index.js
@@ -0,0 +1,2 @@
+import App from '$components/App/App.jsx';
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+var Dotenv = require('dotenv-webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.jsx',
+    vendor: [
+      'react',
+      'react-dom'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(svg|gif|png|jpg|jpeg)$/,
+        use: {
+          loader: 'file-loader'
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    alias: {
+      '$components': path.resolve(__dirname, 'src/components')
+    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.min.css'
+    }),
+    new Dotenv()
+  ]
+};
